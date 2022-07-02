f383933fce04567b7c9ed5173e3b060898975512
yuanh <517493406@qq.com>
2022-07-02 20:17:09 +0800

    删除业务层使用的new方法  解除耦合性 DI入门案列

--- src/main/java/com/yuanh/App.java
+++ src/main/java/com/yuanh/App.java
@@ -10,9 +10,9 @@ public class App {
         //3.获取Ioc容易
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         //4.获取bean 你xml id是啥名你就那啥名字  dao层的
-        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+       /* BookDao bookDao = (BookDao) ctx.getBean("bookDao");
         //调用方法
-        bookDao.save();
+        bookDao.save();*/
 
         //5.获取service的bean
         BookService bookService = (BookService) ctx.getBean("bookService");
--- src/main/java/com/yuanh/service/impl/BookServiceImpl.java
+++ src/main/java/com/yuanh/service/impl/BookServiceImpl.java
@@ -1,9 +1,19 @@
 package com.yuanh.service.impl;
 
+import com.yuanh.dao.BookDao;
 import com.yuanh.service.BookService;
 
 public class BookServiceImpl implements BookService {
+    //5.删除 业务层中使用的new的方式创建dao对象
+    private BookDao bookDao;
     public void save(){
+
         System.out.println("book service save ...");
+        bookDao.save();
+    }
+
+    //6.提供对应的set方法
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
 }
