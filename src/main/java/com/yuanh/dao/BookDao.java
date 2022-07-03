35c14a875acef0b04f7f8913b8dbc7987fc2af4e
yuanh <517493406@qq.com>
2022-07-03 09:26:08 +0800

    实列化bean的三种方法-构造方法(常用)

--- src/main/java/com/yuanh/dao/BookDao.java
+++ src/main/java/com/yuanh/dao/BookDao.java
@@ -1,5 +1,7 @@
 package com.yuanh.dao;
-
+/**
+ * 2.在次基础上参与三种bean的实列化
+ */
 public interface BookDao {
     public void save();
 }
--- src/main/java/com/yuanh/dao/impl/BookDaoImpl.java
+++ src/main/java/com/yuanh/dao/impl/BookDaoImpl.java
@@ -2,7 +2,16 @@ package com.yuanh.dao.impl;
 
 import com.yuanh.dao.BookDao;
 
+/**
+ * 2.在次基础上参与三种bean的实列化
+ */
 public class BookDaoImpl implements BookDao {
+    //构造方法 spring创建bean的时候是，调构造的无参的
+    //实列化bean构造方法（常用）
+    public BookDaoImpl() {
+        System.out.println("book dao constructor is running ....");
+    }
+
     public void save() {
         System.out.println("book dao save ...");
     }
