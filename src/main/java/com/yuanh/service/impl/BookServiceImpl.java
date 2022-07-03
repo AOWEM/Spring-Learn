656049005d8df1a5d79bc390e8c19fc4f14ad67a
yuanh <517493406@qq.com>
2022-07-03 11:23:27 +0800

    spring提供的生命周期方式

--- src/main/java/com/yuanh/service/impl/BookServiceImpl.java
+++ src/main/java/com/yuanh/service/impl/BookServiceImpl.java
@@ -2,8 +2,11 @@ package com.yuanh.service.impl;
 
 import com.yuanh.dao.BookDao;
 import com.yuanh.service.BookService;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class BookServiceImpl implements BookService {
+//spring给你提供的bean生命周期 初始话和销毁方法 InitializingBean DisposableBean
+public class BookServiceImpl implements BookService , InitializingBean, DisposableBean {
     //5.删除 业务层中使用的new的方式创建dao对象
     private BookDao bookDao;
     public void save(){
@@ -14,6 +17,18 @@ public class BookServiceImpl implements BookService {
 
     //6.提供对应的set方法
     public void setBookDao(BookDao bookDao) {
+        System.out.println("setBookDao .....");
         this.bookDao = bookDao;
+    }
+
+    //spring给你提供的bean生命周期 初始话和销毁方法
+    public void destroy() throws Exception {
+        System.out.println("service destroy ....");
+    }
+
+    //spring给你提供的bean生命周期 初始话和销毁方法
+    //当属性设置完了才执行
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("service init ....");
     }
 }
