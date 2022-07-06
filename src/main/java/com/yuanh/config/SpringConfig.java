1c7091934f8c61829863ff007ee78f81d898e32e
yuanh <517493406@qq.com>
2022-07-06 11:31:13 +0800

    依赖注入的自动装配 PropertySource 属性源加载外部文件properties

--- src/main/java/com/yuanh/config/SpringConfig.java
+++ src/main/java/com/yuanh/config/SpringConfig.java
@@ -2,14 +2,17 @@ package com.yuanh.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 纯注解开发bean
  * 可以代替spring的配置文件
  * @Configuration 这是个配置类
  * @ComponentScan 扫描
+ * @PropertySource 属性源加载外部文件properties
  */
 @Configuration
 @ComponentScan({"com.yuanh.service","com.yuanh.dao"})
+@PropertySource("jdbc.properties")
 public class SpringConfig {
 }
